63ca8494e2c7c1752c243f33db89a5390c42bd38
arthikareddykoduru-cmd <arthikareddykoduru@gmail.com>
2026-07-19 14:15:25 +0530

    Delete DSA directory

--- DSA/forecasting/forecast.java
+++
@@ -1,9 +0,0 @@
-package DSA.forecasting;
-public class Forecast {
-    public static double predictFutureValue(double currentValue, double growthRate, int years) {
-        if (years == 0) {
-            return currentValue; // base case
-        }
-        return predictFutureValue(currentValue * (1 + growthRate), growthRate, years - 1);
-    }
-}
